7d344ad23a3beb476c4221ba0dace3350dd42471
hanyi <2385931432@qq.com>
2022-11-29 07:33:59 +0800

    p33-39

---
+++ day02/src/main/java/com/itheima/_08接口的基本实现/InterfaceDemo.java
@@ -0,0 +1,54 @@
+package com.itheima._08接口的基本实现;
+
+/**
+ * 目标：接口的基本实现
+ * <p>
+ * 子类       继承    父类
+ * 实现类     实现     接口
+ * 类与类是继承关系
+ * 类与接口是实现关系，接口是被类实现的。
+ * 实现接口的类称为：实现类。
+ * <p>
+ * 类实现接口的格式：
+ * 修饰符 class 实现类名称 implements 接口1，接口2、、、{
+ * <p>
+ * }
+ * implements：类实现接口的关键字。
+ *
+ * 小结：
+ *  一个类实现接口，这个类称为实现类。
+ *  接口是用来被实现的，实现的关键字是implements，接口可以多实现。
+ *  一个类实现接口必须重写完接口中全部的抽象方法，否则这个类要定义成抽象类。
+ */
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        PingPongMan pingPongMan = new PingPongMan("张继科");
+        pingPongMan.competition();
+        pingPongMan.run();
+    }
+
+}
+
+class PingPongMan implements SportMan {
+    private String name;
+
+    public PingPongMan(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(name + "要跑步！");
+    }
+
+    @Override
+    public void competition() {
+        System.out.println(name + "参加比赛！");
+    }
+}
+
+interface SportMan{
+    void run();
+
+    void competition();
+}
---
+++ day02/src/main/java/com/itheima/_09接口的多实现/InterfaceDemo.java
@@ -0,0 +1,51 @@
+package com.itheima._09接口的多实现;
+
+/**
+ * 目标：接口的多实现研究。
+ *
+ * 类与类是单继承关系。
+ * 类与接口是多实现关系。
+ *
+ * 类实现接口的格式：
+ * 修饰符 class 实现类名称 implements 接口1，接口2、、、{
+ *
+ * }
+ * implements：类实现接口的关键字。
+ *
+ * 小结：
+ *  一个类实现多个接口，必须重写完全部接口中的全部抽象方法，否则这个类要定义成抽象类。
+ */
+public class InterfaceDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
+class BasketBall implements SportMan, Law {
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void competition() {
+
+    }
+
+    @Override
+    public void rule() {
+
+    }
+}
+
+
+interface SportMan {
+    void run();
+
+    void competition();
+}
+
+interface Law {
+    void rule();
+}
