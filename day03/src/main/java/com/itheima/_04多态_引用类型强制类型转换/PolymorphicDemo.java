3010b3132d4cb429fb4a781204a01672bbf846c3
hanyi <2385931432@qq.com>
2022-12-23 22:24:29 +0800

    p56

--- day03/src/main/java/com/itheima/_04多态_引用类型强制类型转换/PolymorphicDemo.java
+++ day03/src/main/java/com/itheima/_04多态_引用类型强制类型转换/PolymorphicDemo.java
@@ -13,7 +13,67 @@ package com.itheima._04多态_引用类型强制类型转换;
  * 但是运行阶段可能出现：类型转换异常ClassCastException
  *
  * Java建议在进行强制类型转换之前先判断变量的真是类型，在强制类型转换！
- *  变量 instanceof 类型：判断前面的变量是否是后面的类型或者其子类类型。
+ *  变量 instanceof 类型：判断前面的变量是否是后面的类型或者其子类类型才会返回true。
+ *
+ * 小结：有继承/实现关系的两个类型就可以进行强制类型转换，编译阶段一定不报错！
+ * 但是运行阶段可能出现：类型转换异常ClassCastException
  */
 public class PolymorphicDemo {
+    public static void main(String[] args) {
+        Animal a = new Wolf();
+        a.run();
+        //a.catchSheep();
+
+        // 1.把动物类型的变量a转换成真是的狼类型
+        Wolf w = (Wolf) a;
+        w.catchSheep();
+
+        // 2.多态下类型转换异常问题研究（重点）
+        Animal a1 = new Cat();
+        Wolf w1 = (Wolf) a1; // 编译阶段没有报错！在运行阶段可能出现ClassCastException类转换成异常！
+        w1.catchSheep();
+
+        if (a1 instanceof Cat) {
+            Cat cat = (Cat) a1;
+            cat.run();
+        } else if (a1 instanceof Wolf) {
+            Wolf w2 = (Wolf) a1;
+            w2.run();
+        }
+    }
+
+    public static void go(Animal animal) {
+        System.out.println("开始！");
+        animal.run();
+        System.out.println("结束！");
+    }
+}
+
+class Wolf extends Animal {
+    @Override
+    public void run() {
+        System.out.println("狼跑的飞快！");
+    }
+
+    public void catchSheep() {
+        System.out.println("狼抓羊");
+    }
+}
+
+class Cat extends Animal {
+
+    @Override
+    public void run() {
+        System.out.println("猫跑的贼快！");
+    }
+
+    public void catchMouse() {
+        System.out.println("猫抓老鼠！");
+    }
+}
+
+class Animal{
+    public void run() {
+        System.out.println("动物可以跑！");
+    }
 }
