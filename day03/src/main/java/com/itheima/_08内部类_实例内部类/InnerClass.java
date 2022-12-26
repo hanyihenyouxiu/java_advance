78b89acccdfd5478182c1dab4cae56671f99e71b
hanyi <2385931432@qq.com>
2022-12-26 23:16:24 +0800

    p81-82

--- day03/src/main/java/com/itheima/_08内部类_实例内部类/InnerClass.java
+++ day03/src/main/java/com/itheima/_08内部类_实例内部类/InnerClass.java
@@ -1,49 +1,38 @@
 package com.itheima._08内部类_实例内部类;
 
 /**
- * 目标： 内部类_实例内部类（成员内部类）
+ * 目标：内部类_实例内部类（成员内部类）
  *
  * 什么是实例内部类：
- *  无static修饰的内部类，属于外部类的的每个对象的，随着对象的创建一起加载。
+ *  无static修饰的内部类，属于外部类的每个对象的，与对象一起加载的。
  *
- * 实例内部类的成分特点：
+ * 实例内部类的成员特点：
  *  实例内部类中不能定义静态成员，其他都可以定义。
  *  可以定义常量
  *
- * 实例内部类的访问格式：
+ * 成员内部类访问格式：
  *  外部类名称.内部类名称
  *
- * 创建对象的格式：
- *  外部类名称.内部类名称 对象名 = new 外部类名称.new 内部类构造器
+ * 创建对象的格式
+ *  外部类名称，内部类名称 对象名 = new 外部类构造器.new 内部类构造器；
  *
- * 拓展：
- *  实例内部类中是否可以直接访问外部类的静态成员？ 可以的，外部类的静态成员可以被共享访问！
- *  实例内部类中是否可以访问外部类的实例成员？ 可以的，实例内部类属于外部类对象，可以直接访问当前外部类的实例成员！
+ * 实例内部类的访问扩展：
+ *  实例内部类中是否可以直接访问外部类的静态成员？
  *
- * 小结：
- *  实例内部类书与外部类，需要用外部类一起加载，
- *  实例内部类可以访问外部类的全部成员！
  */
 public class InnerClass {
-    public static void main(String[] args) {
-        Outer.Inner obj = new Outer().new Inner();
-        obj.show();
-    }
 }
 
 // 外部类
 class Outer {
 
-    public static int age = 1;
-    public String saa = "123";
+    // 内部类
+    public static class Inner {
 
-    // 实例内部类：无static修饰，属于外部类的对象
-    public class Inner {
         private String name;
 
-        public void show() {
-            System.out.println(age);
-            System.out.println(saa);
+        private void show() {
+
         }
 
         public String getName() {
---
+++ day04/src/main/java/com/itheima/_07正则表达式的演示/RegexDemo.java
@@ -0,0 +1,40 @@
+package com.itheima._07正则表达式的演示;
+
+/**
+ * 目标：正则表达式的演示
+ *
+ * 正则表达式的作用：
+ *  是一些特殊字符组成的校验规则，可以校验信息的正确性，校验邮箱是否合法，电话号码，金额等。
+ *
+ * 需求：演示不用正则表达式和用正则表达式校验qq号码。
+ */
+public class RegexDemo {
+    public static void main(String[] args) {
+        System.out.println("32321431");
+    }
+
+    // 不用正则表达式
+    public static boolean checkQQ(String args) {
+        if (args == null) {
+            return false;
+        } else {
+            if (args.length() >= 4) {
+                boolean rs = true;
+                for (int i = 0; i < args.length(); i++) {
+                    char ch = args.charAt(i);
+                    if (ch < '0' || ch > '9') {
+                        rs = false;
+                    }
+                }
+                return rs;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    // 使用正则表达式
+    public static boolean checkQQRegex(String qq) {
+        return qq != null && qq.matches("\\d{4,}");
+    }
+}
