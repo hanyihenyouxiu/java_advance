164989cdfbe14251506b5d90440057e3bc90bde5
王绍宁 <i2385931432@163.com>
2023-01-10 09:09:38 +0800

    p164

---
+++ day08/src/main/java/com/itheima/_01线程通信/DrawThread.java
@@ -0,0 +1,32 @@
+package com.itheima._01线程通信;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 寒毅
+ * Date: 2023/1/9
+ * Time: 14:09
+ * Description:
+ *
+ * @author 寒毅
+ */
+public class DrawThread extends Thread {
+
+    private Account account;
+
+    public DrawThread(String name, Account account) {
+        super(name);
+        this.account = account;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(3000);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            account.drawMoney(10000d);
+        }
+    }
+}
