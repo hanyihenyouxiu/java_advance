26bfd52bc27f6423849d44e60a2e9b3e71ac57c2
王绍宁 <i2385931432@163.com>
2023-01-13 15:54:43 +0800

    refactor:p217

--- day09/src/main/java/com/hanyi/_25字节流的使用/FileOutputStreamDemo01.java
+++ day09/src/main/java/com/hanyi/_25字节流的使用/FileOutputStreamDemo01.java
@@ -12,15 +12,11 @@ import java.util.Arrays;
 public class FileOutputStreamDemo01 {
 
     public static void main(String[] args) throws Exception{
-        File fi = new File("day09/src/main/java/com/hanyi/_25字节流的使用/a.txt");
         File fo = new File("day09/src/main/java/com/hanyi/_25字节流的使用/b.txt");
-        InputStream in = Files.newInputStream(fi.toPath());
         OutputStream ou = Files.newOutputStream(fo.toPath());
-        int length;
-        byte[] bytes = new byte[4];
-        while ((length = in.read(bytes)) != -1) {
-            System.out.println(new String(bytes, 0, length));
-            ou.write(bytes, 0 , length);
-        }
+        ou.write('c');
+        ou.write('v');
+        ou.flush();
+        ou.close();
     }
 }
