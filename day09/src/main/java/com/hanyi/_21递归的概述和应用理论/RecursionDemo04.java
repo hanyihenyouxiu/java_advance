789eed1adc03d4e9c643dbfecaccb27ccc389928
hanyi <2385931432@qq.com>
2023-01-14 16:32:19 +0800

    fix:p208-210

--- day09/src/main/java/com/hanyi/_21递归的概述和应用理论/RecursionDemo04.java
+++ day09/src/main/java/com/hanyi/_21递归的概述和应用理论/RecursionDemo04.java
@@ -1,15 +1,24 @@
 package com.hanyi._21递归的概述和应用理论;
 
 /**
- * f(x) = f(x-1) * 1.5 + y
- * f(1) = 1.5 + y
+ * f(x) = 1.5x + 1
+ * f(1) = 1.5 * 1 + 1
+ * f(2) = 1.5 * 2 + 1
+ * 递归三要素：
+ *      公式：
+ *      终结点：
+ *      递归的方向：
  */
 public class RecursionDemo04 {
     public static void main(String[] args) {
-        System.out.println(f(10, 0.5));
+        System.out.println(f(10));
     }
 
-    public static double f(double x, double y) {
-        return 1;
+    public static double f(double x) {
+        if (x == 1) {
+            return 2.5;
+        } else {
+            return 1.5 + f(x - 1);
+        }
     }
 }
--- day09/src/main/java/com/hanyi/_21递归的概述和应用理论/RecursionDemo05.java
+++ day09/src/main/java/com/hanyi/_21递归的概述和应用理论/RecursionDemo05.java
@@ -5,6 +5,10 @@ package com.hanyi._21递归的概述和应用理论;
  */
 public class RecursionDemo05 {
     public static void main(String[] args) {
+        System.out.println(f(10));
+    }
 
+    public static int f(int x) {
+        return x == 1 ? 1 : f(x - 1) + x;
     }
 }
